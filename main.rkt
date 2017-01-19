eac48534158e1e1e377fbdf34f6f1cf361ddde52
Saurav Keshari Aryal <saurav.aryal@bison.howard.edu>
2017-01-19 18:36:46 -0500

    tried modifying the simple sum, ran into an error, need to simlify

--- main.rkt
+++ main.rkt
@@ -15,3 +15,14 @@
          ((LIST? (CAR L)) (sum-up-numbers-simple (CDR L)))
          ((NUMBER? (CAR L)) (+(CAR L) (sum-up-numbers-simple (CDR L))))
          (ELSE (sum-up-numbers-simple (CDR L)))))
+
+;;; This procedure takes a list L as an arguement.
+;;; Returns the general sum of numbers including values in sub-lists in L.
+(DEFINE (sum-up-numbers-general L)
+        (COND
+         ((NULL? L) 0)
+         ((LIST? (CAR L)) (+ sum-up-numbers-general (CAR L) sum-up-numbers-general (CDR L)))
+         ((NUMBER? (CAR L)) (+(CAR L) (sum-up-numbers-general (CDR L))))
+         (ELSE (sum-up-numbers-general (CDR L)))))
+
+(sum-up-numbers-general '(a 100 ((b ((200) c)) 300 d)))
