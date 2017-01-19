9e49896dd61487953e8a17875b3152685b508401
Saurav Keshari Aryal <saurav.aryal@bison.howard.edu>
2017-01-19 15:04:18 -0500

    Commiting tests for reverse-general, one test case seems to be incorrect

--- test.rkt
+++ test.rkt
@@ -1,3 +1,13 @@
 (load "main.rkt")
+(#%require schemeunit)
 
-(reverse-general '(A (B C)))
+;;; Unit Test Cases for reverse-general
+(check-equal? (reverse-general '()) '())
+(check-equal? (reverse-general '(A B C)) '(C B A))
+(check-equal? (reverse-general '(A B ())) '(() B A))
+(check-equal? (reverse-general '((A B C))) '((C B A)))
+(check-equal? (reverse-general '((A B C) (D E F))) '((F E D) (C B A)))
+(check-equal? (reverse-general '(A (B C) ((D E) F) G)) '(G ((F (E D) (C B) A))))
+(check-equal? (reverse-general '(1 (2 3) (4 (A (B (C D)))))) '(((((D C) B) A) 4) (3 2) 1))
+
+(DISPLAY "All test cases pass!")
