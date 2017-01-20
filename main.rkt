abb7e8e58103b66571e99f25593c5851508cc469
Saurav Keshari Aryal <saurav.aryal@bison.howard.edu>
2017-01-19 19:07:42 -0500

    Added comments for sum general

--- main.rkt
+++ main.rkt
@@ -17,9 +17,13 @@
          (ELSE (sum-up-numbers-simple (CDR L)))))
 
 
+;;; This procedure takes a list L as an arguement.
+;;; Returns the sum of the numbers even within the sublists of L.
 (DEFINE (sum-up-numbers-general L)
         (COND
          ((NULL? L) 0)
          ((LIST? (CAR L))(+ (sum-up-numbers-general (CAR L)) (sum-up-numbers-general (CDR L))))
          ((NUMBER? (CAR L)) (+(CAR L) (sum-up-numbers-general (CDR L))))
          (ELSE (sum-up-numbers-general (CDR L)))))
+
+
